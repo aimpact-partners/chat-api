fb5d98a74b180a112c50e962d61fba77cb80274c
Julio Rodriguez <jircdev@gmail.com>
2023-06-08 14:10:43 -0300

    Feat: Add structure of models

--- modules/entities/models/ts/chat/item.ts
+++ modules/entities/models/ts/chat/item.ts
@@ -1,6 +1,6 @@
 // ChatItem
-import {Item} from '@beyond-js/reactive-2/entities';
-import {ChatProvider} from '@aimpact/chat-api/provider';
+import { Item } from '@beyond-js/reactive-2/entities';
+import { ChatProvider } from '@aimpact/chat-api/provider';
 
 interface IChat {
 	userId: string;
@@ -9,11 +9,10 @@ interface IChat {
 
 export /*bundle*/ class Chat extends Item<IChat> {
 	protected properties = ['id', 'userId', 'category'];
-	protected storeName = 'chat';
+	protected storeName = 'Chat';
 	protected db = 'chat-api@1';
 
-	constructor({id = undefined} = {}) {
-		super({id});
-		this.provider = new ChatProvider();
+	constructor({ id = undefined } = {}) {
+		super({ id, provider: ChatProvider });
 	}
 }
--- modules/entities/models/ts/documents/collection.ts
+++ modules/entities/models/ts/documents/collection.ts
@@ -1,19 +1,17 @@
-
-¿
 import { Collection } from '@beyond-js/reactive-2/entities';
-import { Document } from './document';
+import { Document } from './item';
 
 interface IDocuments {
-    items: Document[];
+	items: Document[];
 }
 
 export class Documents extends Collection {
-    item = Document;
-    protected storeName = 'Documents';
-    protected db = 'chat-api@1';
+	item = Document;
+	protected storeName = 'Documents';
+	protected db = 'chat-api@1';
 
-    constructor() {
-        super();
-        this.init();
-    }
+	constructor() {
+		super();
+		this.init();
+	}
 }
