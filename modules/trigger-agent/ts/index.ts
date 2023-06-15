91b4ebaa57dde214175ae28ba26668c5f029951b
ftovar8 <felix.godo@gmail.com>
2023-06-14 21:28:33 -0400

    feat: update agent call

--- modules/trigger-agent/ts/index.ts
+++ modules/trigger-agent/ts/index.ts
@@ -1,23 +1,23 @@
 import fetch from 'node-fetch';
 
 export /*bundle*/ class TriggerAgent {
-	#url = 'http://localhost:5010/agent';
-	#options = {
-		method: 'POST',
-		headers: {'Content-Type': 'application/json'},
-	};
+    #url = 'http://localhost:5010/agent';
+    #options = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+    };
 
-	async call(text: string) {
-		try {
-			const options = {...this.#options, body: JSON.stringify({text})};
+    async call(text: string, path: string) {
+        try {
+            const options = { ...this.#options, body: JSON.stringify({ text, path }) };
 
-			const response = await fetch(this.#url, options);
-			const responseJson = await response.json();
+            const response = await fetch(this.#url, options);
+            const responseJson = await response.json();
 
-			return responseJson;
-		} catch (e) {
-			console.error('Error en la petición:', e);
-			return {status: false, error: e.message};
-		}
-	}
+            return responseJson;
+        } catch (e) {
+            console.error('Error en la petición:', e);
+            return { status: false, error: e.message };
+        }
+    }
 }
