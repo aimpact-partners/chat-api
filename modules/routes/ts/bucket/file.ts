613be24f440e5daaca9e5a66bf4b61a124f9e3d8
ftovar8 <felix.godo@gmail.com>
2023-06-30 15:50:25 -0300

    feat: get file bucket

--- modules/routes/ts/bucket/file.ts
+++ modules/routes/ts/bucket/file.ts
@@ -3,19 +3,24 @@ import { Storage } from '@google-cloud/storage';
 import { getFirebaseConfig } from '@aimpact/chat-api/firebase-config';
 
 export class FilestoreFile {
-    private app;
-    private storage;
-    firebaseConfig;
-    constructor() {
-        this.firebaseConfig = getFirebaseConfig();
-        this.app = initializeApp(this.firebaseConfig);
-        this.storage = new Storage();
-    }
+	private app;
+	private storage;
+	firebaseConfig;
+	constructor() {
+		this.firebaseConfig = getFirebaseConfig();
+		this.app = initializeApp(this.firebaseConfig);
+		this.storage = new Storage();
+	}
 
-    async upload(path: string, destination: string): Promise<string> {
-        const bucketName = this.firebaseConfig.storageBucket;
-        await this.storage.bucket(bucketName).upload(path, { destination });
+	getFile(destination: string) {
+		const bucketName = this.firebaseConfig.storageBucket;
+		const file = this.storage.bucket(bucketName).file(destination);
+		return file;
+	}
+	async upload(path: string, destination: string): Promise<string> {
+		const bucketName = this.firebaseConfig.storageBucket;
+		await this.storage.bucket(bucketName).upload(path, { destination });
 
-        return destination;
-    }
+		return destination;
+	}
 }
