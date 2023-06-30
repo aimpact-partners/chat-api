c2d1eee84323b71fdac561171634ce84fe7ede05
ftovar8 <felix.godo@gmail.com>
2023-06-30 15:32:19 -0300

    fix: chat without knowledgeBox

--- modules/backend/provider/bridge/chat/messages.ts
+++ modules/backend/provider/bridge/chat/messages.ts
@@ -5,93 +5,99 @@ import * as admin from 'firebase-admin';
 import { TriggerAgent } from '@aimpact/chat-api/trigger-agent';
 
 interface Message {
-    id: string;
-    chatId: string;
-    message: string;
-    role: string;
-    timestamp: number;
+	id: string;
+	chatId: string;
+	message: string;
+	role: string;
+	timestamp: number;
 }
 
 export class ChatMessages {
-    socket: Server;
-    private collection;
-    private table = 'messages';
-    #agent: TriggerAgent;
+	socket: Server;
+	private collection;
+	private table = 'messages';
+	#agent: TriggerAgent;
 
-    constructor() {
-        this.#agent = new TriggerAgent();
-    }
+	constructor() {
+		this.#agent = new TriggerAgent();
+	}
 
-    async bulkSave(data) {
-        try {
-            const entries = [];
-            const promises = [];
-            data.forEach(item => promises.push(this.collection.add(item)));
-            await Promise.all(promises).then(i => i.map((chat, j) => entries.push({ id: chat.id, ...data[j] })));
+	async bulkSave(data) {
+		try {
+			const entries = [];
+			const promises = [];
+			data.forEach(item => promises.push(this.collection.add(item)));
+			await Promise.all(promises).then(i => i.map((chat, j) => entries.push({ id: chat.id, ...data[j] })));
 
-            return { status: true, data: { entries } };
-        } catch (e) {
-            return { status: false, error: e.message };
-        }
-    }
+			return { status: true, data: { entries } };
+		} catch (e) {
+			return { status: false, error: e.message };
+		}
+	}
 
-    async publish(data) {
-        try {
-            if (!data.chatId) {
-                throw new Error('chatId is required');
-            }
-            if (!data.content) {
-                throw new Error('message is required');
-            }
+	async publish(data) {
+		try {
+			if (!data.chatId) {
+				throw new Error('chatId is required');
+			}
+			if (!data.content) {
+				throw new Error('message is required');
+			}
 
-            const chatProvider = db.collection('Chat');
-            const chat = await chatProvider.doc(data.chatId);
-            const chatDoc = await chat.get();
-            const chatData = chatDoc.data();
+			const chatProvider = db.collection('Chat');
+			const chat = await chatProvider.doc(data.chatId);
+			const chatDoc = await chat.get();
+			const chatData = chatDoc.data();
 
-            const KBProvider = db.collection('KnowledgeBoxes');
-            const KB = await KBProvider.doc(chatData.knowledgeBoxId);
-            const KBDoc = await KB.get();
-            const KBData = KBDoc.data();
+			let knowledgeBoxId;
+			if (chatData.knowledgeBoxId) {
+				const KBProvider = db.collection('KnowledgeBoxes');
+				const KB = await KBProvider.doc(chatData.knowledgeBoxId);
+				const KBDoc = await KB.get();
+				const KBData = KBDoc.data();
+				if (!KBData) {
+					return { status: false, error: 'knowledgeBox not exist' };
+				}
+				knowledgeBoxId = KBData.id;
+			}
 
-            const { prompt } = data;
-            delete data.prompt;
-            const response = await this.#agent.call(data, chatData.id, prompt, KBData?.id);
+			const { prompt } = data;
+			delete data.prompt;
+			const response = await this.#agent.call(data, chatData.id, prompt, knowledgeBoxId);
+			if (!response.status) {
+				return response;
+			}
 
-            if (!response.status) {
-                return response;
-            }
+			/**
+			 * user message
+			 */
+			await chat
+				.collection(this.table)
+				.doc(data.id)
+				.set({
+					...data,
+					timestamp: admin.firestore.FieldValue.serverTimestamp(),
+				});
+			const savedMessage = await chat.collection(this.table).doc(data.id).get();
+			const responseData = savedMessage.exists ? savedMessage.data() : undefined;
 
-            /**
-             * user message
-             */
-            await chat
-                .collection(this.table)
-                .doc(data.id)
-                .set({
-                    ...data,
-                    timestamp: admin.firestore.FieldValue.serverTimestamp(),
-                });
-            const savedMessage = await chat.collection(this.table).doc(data.id).get();
-            const responseData = savedMessage.exists ? savedMessage.data() : undefined;
+			/**
+			 * agent message
+			 */
+			const agentMsgId = uuidv4();
+			const agentMessage = {
+				id: agentMsgId,
+				chatId: data.chatId,
+				content: response.data.output,
+				role: 'system',
+				timestamp: admin.firestore.FieldValue.serverTimestamp(),
+			};
+			await chat.collection(this.table).doc(agentMsgId).set(agentMessage);
 
-            /**
-             * agent message
-             */
-            const agentMsgId = uuidv4();
-            const agentMessage = {
-                id: agentMsgId,
-                chatId: data.chatId,
-                content: response.data.output,
-                role: 'system',
-                timestamp: admin.firestore.FieldValue.serverTimestamp(),
-            };
-            await chat.collection(this.table).doc(agentMsgId).set(agentMessage);
-
-            return { status: true, data: { message: responseData, response: agentMessage } };
-        } catch (e) {
-            console.error(e);
-            return { status: false, error: e.message };
-        }
-    }
+			return { status: true, data: { message: responseData, response: agentMessage } };
+		} catch (e) {
+			console.error(e);
+			return { status: false, error: e.message };
+		}
+	}
 }
