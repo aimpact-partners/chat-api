7844408a1f929cf5219fcc8ffe43d7874ec23438
ftovar8 <felix.godo@gmail.com>
2023-07-02 17:31:57 -0300

    feat:

--- modules/routes/ts/index.ts
+++ modules/routes/ts/index.ts
@@ -1,7 +1,9 @@
 import { uploader } from './routes/uploader';
+import { token } from './routes/token';
 
 export /*bundle*/
 function routes(app) {
 	app.get('/', (req, res) => res.send('AImpact Chat API http server'));
 	app.post('/upload', uploader);
+	app.post('/token', token);
 }
