4a47fe64458a2acc24282e5e3022985c4d3973c3
ftovar8 <felix.godo@gmail.com>
2023-07-03 12:01:04 -0300

    feat: response user on login - set headers on upload cloud function

--- modules/cloud-functions/start/ts/start.ts
+++ modules/cloud-functions/start/ts/start.ts
@@ -3,4 +3,14 @@ import { uploader } from '@aimpact/chat-api/routes';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-process.env.FUNCTION_REGION && functions.http('chat-api-uploader', uploader);
+const setHeaders = res => {
+	res.set('Access-Control-Allow-Origin', '*');
+	res.set('Access-Control-Allow-Methods', 'POST');
+	res.set('Access-Control-Allow-Headers', 'Content-Type');
+	res.set('Access-Control-Max-Age', '3600');
+};
+const run = (req, res) => {
+	setHeaders(res);
+	uploader(req, res);
+};
+process.env.FUNCTION_REGION && functions.http('chat-api-uploader', run);
--- modules/entities/models/ts/users/item.ts
+++ modules/entities/models/ts/users/item.ts
@@ -3,30 +3,30 @@ import { Item } from '@beyond-js/reactive/entities';
 import { UserProvider } from '@aimpact/chat-api/backend-provider';
 
 interface IUser {
-    id: string;
-    displayName: string;
-    email: string;
-    photoURL: string;
-    phoneNumber: string;
+	id: string;
+	displayName: string;
+	email: string;
+	photoURL: string;
+	phoneNumber: string;
 }
 
 export /*bundle*/ class User extends Item<IUser> {
-    protected properties = ['displayName', 'id', 'email', 'photoURL', 'phoneNumber'];
+	protected properties = ['displayName', 'id', 'email', 'photoURL', 'phoneNumber'];
 
-    #logged;
-    get logged() {
-        return this.#logged;
-    }
-    constructor({ id = undefined } = {}) {
-        super({ id, db: 'chat-api', storeName: 'User', provider: UserProvider });
-    }
+	#logged;
+	get logged() {
+		return this.#logged;
+	}
+	constructor({ id = undefined } = {}) {
+		super({ id, db: 'chat-api', storeName: 'User', provider: UserProvider });
+	}
 
-    async login(data) {
-        await this.isReady;
-        if (this.#logged) return;
-        await this.set(data.user);
-        await this.provider.updateUser({ ...this.getProperties(), id: this.id });
-        this.#logged = true;
-        return true;
-    }
+	async login(data) {
+		await this.isReady;
+		if (this.#logged) return;
+		await this.set(data.user);
+		await this.provider.updateUser({ ...this.getProperties(), id: this.id });
+		this.#logged = true;
+		return true;
+	}
 }
