220f8991ba6cb264af5076c7e6996afcfef15253
=Julio Rodriguez <jircdev@gmail.com>
2023-07-05 14:23:26 -0300

    feat: Implement real-time connection management with provider, backend, and socket properties
    This commit introduces the following changes:
    - Added a provider class to manage real-time connections.
    - Exposed backend and socket properties in the provider class.
    - Integrated the model with the view and implemented general UI/UX enhancements.

--- modules/entities/models/ts/classes/item.ts
+++ modules/entities/models/ts/classes/item.ts
@@ -1,14 +1,14 @@
 import { Item } from '@beyond-js/reactive/entities';
-
+import { ClassesProvider } from '@aimpact/chat-api/backend-provider';
 interface IClass {
 	title: string;
 	objectives: string;
 }
 
 export /*bundle*/ class Class extends Item<IClass> {
-	protected properties = ['title', 'objectives'];
+	protected properties = ['curriculumObjective', 'topics'];
 
 	constructor({ id = undefined } = {}) {
-		super({ id, db: 'chat-api', storeName: 'Classes' });
+		super({ id, db: 'chat-api', storeName: 'Classes', provider: ClassesProvider });
 	}
 }
