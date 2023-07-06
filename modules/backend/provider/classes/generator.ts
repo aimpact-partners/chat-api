0a72ec5eec339897dfe721d1302fe7b4e69209ef
hello-beyond <94199486+hello-beyond@users.noreply.github.com>
2023-07-06 12:50:05 +0000

    Class generation adjustments

--- modules/backend/provider/classes/generator.ts
+++ modules/backend/provider/classes/generator.ts
@@ -1,90 +1,28 @@
 import type { Socket } from 'socket.io';
-import { Configuration, OpenAIApi, ChatCompletionRequestMessage } from 'openai';
-import * as dotenv from 'dotenv';
-import { prompts } from './prompts';
+import { Prompts, GenerationParams } from './prompts';
 
-dotenv.config();
-
-const LOCAL = !process.env.FUNCTION_REGION;
-const config = new Configuration({ apiKey: process.env.OPEN_AI_KEY });
-const openai = new OpenAIApi(config);
-
-type PromptExecutionResponse = { status: boolean, response?: string, error?: string, format: string, prompt: string };
-
-const executePrompt: (prompt: string, format?: string) => Promise<PromptExecutionResponse> = async (prompt, format) => {
-	format = format === 'json' ? 'json' : 'text';
-
-	const messages: ChatCompletionRequestMessage[] = [{
-		role: 'user',
-		content: prompt
-	}];
-
-	try {
-		const response = await openai.createChatCompletion({
-			model: 'gpt-4',
-			messages,
-			temperature: 0.2,
-		});
-
-		let { content } = response.data.choices[0].message;
-
-		// If format is 'json', check if it can be parsed
-		if (format === 'json') {
-			try {
-				JSON.stringify(JSON.parse(content));
-			}
-			catch (exc) {
-				console.warn(`JSON cannot be parsed:\n${content}`)
-
-				// TODO: Probably it would be required to scrap the json inside the message
-				const error = `It was expected a JSON, but it couldn't be parsed`;
-				return { status: false, response: content, error, format: 'text', prompt };
-			}
-		}
-
-		return { status: true, response: content, format, prompt };
-	} catch (e) {
-		console.error(e.stack);
-		return { status: false, error: e.message, format: 'text', prompt };
-	}
+export async function generate(curriculumObjective: string, params: GenerationParams, socket: Socket) {
+	const prompts = new Prompts(curriculumObjective, socket);
+	return prompts.execute(Object.assign(params));
 }
 
-export async function generator(curriculumObjective: string, topics: string[], socket: Socket) {
+export async function generateAll(curriculumObjective: string, topics: string[], socket: Socket) {
 	const response = { class: { synthesis: void 0, assessment: void 0 }, topics: [] };
 
-	const emit = (element: string, is: string, done: boolean, topic?: string) => {
-		socket?.emit('class-generation', { element, is, done, topic });
+	const prompts = new Prompts(curriculumObjective, socket);
 
-		const generation = done ? 'generation done' : 'generation started';
-		LOCAL && console.log(`${element} ${is} ${generation}`);
-	}
-
-	const process = async (element: string, is: string, topic?: string, format?: string): Promise<PromptExecutionResponse> => {
-		if (!['class', 'topic'].includes(element)) throw new Error('Parameter "element" is invalid');
-		if (!['assessment', 'synthesis', 'preRequisitesAssessment'].includes(is)) throw new Error('Parameter "is" is invalid');
-
-		emit(element, is, false, topic);
-
-		const prompt = prompts[element][is](curriculumObjective, topic ? topic : topics);
-		const processed = await executePrompt(prompt, format);
-
-		emit(element, is, true, topic);
-
-		return processed;
-	}
-
-	response['class'].synthesis = await process('class', 'synthesis');
-	response['class'].assessment = await process('class', 'assessment', void 0, 'json');
+	response['class'].synthesis = await prompts.execute({ is: 'class', element: 'synthesis', topics });
+	response['class'].assessment = await prompts.execute({ is: 'class', element: 'assessment', format: 'json' });
 
 	for (const topic of topics) {
-		const synthesis = await process('topic', 'synthesis');
-		const preRequisitesAssessment = await process('topic', 'preRequisitesAssessment', 'json');
-		const assessment = await process('topic', 'assessment', 'json');
+		const synthesis = await prompts.execute({ is: 'topic', element: 'synthesis', topic });
+		const previous = await prompts.execute({ is: 'topic', element: 'previous', topic, format: 'json' });
+		const assessment = await prompts.execute({ is: 'topic', element: 'assessment', topic, format: 'json' });
 
 		response.topics.push({
 			topic,
 			synthesis,
-			preRequisitesAssessment,
+			previous,
 			assessment
 		});
 	}
