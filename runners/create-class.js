845eedd1fce39d15c24d85fee78b9cb9c262657a
=Julio Rodriguez <jircdev@gmail.com>
2023-07-13 14:37:38 -0300

    chore: rename packages based on the new structure

--- runners/create-class.js
+++ runners/create-class.js
@@ -6,11 +6,11 @@ const curriculumObjective = 'Los primeros pobladores de América';
 const topics = ['Hipótesis Clovis', 'Desafío Monteverde a Hipótesis Clovis'];
 
 (async () => {
-    const { ClassesProvider } = await bimport("@aimpact/chat-api/backend-provider")
-    const course = new ClassesProvider();
+	const { ClassesProvider } = await bimport('@aimpact/chat-api/provider');
+	const course = new ClassesProvider();
 
-    const params = { is: 'class', element: 'synthesis', topics };
-    const {prompt, response} = await course.generate(curriculumObjective, params);
-    console.log(`PROMPT:\n${prompt}\n\n`);
-    console.log(`RESPONSE:\n${response}`);
+	const params = { is: 'class', element: 'synthesis', topics };
+	const { prompt, response } = await course.generate(curriculumObjective, params);
+	console.log(`PROMPT:\n${prompt}\n\n`);
+	console.log(`RESPONSE:\n${response}`);
 })();
