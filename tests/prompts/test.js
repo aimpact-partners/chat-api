0a685b677ea45878cebe052b8fcc3dc041690252
ftovar8 <felix.godo@gmail.com>
2023-11-03 14:27:19 -0300

    feat: tests endpoints

--- tests/prompts/test.js
+++ tests/prompts/test.js
@@ -1,8 +1,17 @@
-describe('run API', () => {
-	test('tes endpoint ', async () => {
-		const body = JSON.stringify({ prompt: 'dame un saludo motivacional' });
-		const options = { method: 'POST', headers: { 'Content-Type': 'application/json' }, body };
-		const url = 'https://chat-api-http-v2-rb5caohzgq-uc.a.run.app/prompts/templates/process';
+describe('run Prompts API', () => {
+	// test('tes endpoint ', async () => {
+	// 	const body = JSON.stringify({ prompt: 'dame un saludo motivacional de 5 palabras' });
+	// 	const options = { method: 'POST', headers: { 'Content-Type': 'application/json' }, body };
+	// 	const url = 'https://chat-api-http-v2-rb5caohzgq-uc.a.run.app/prompts/templates/process';
+	// 	const response = await fetch(url, options);
+
+	// 	const { error, data } = await response.json();
+	// 	error ? console.error(error) : console.log(data);
+	// });
+
+	test('test endpoint ', async () => {
+		const options = { method: 'GET' };
+		const url = 'http://localhost:5040/prompts/templates';
 		const response = await fetch(url, options);
 
 		const { error, data } = await response.json();
