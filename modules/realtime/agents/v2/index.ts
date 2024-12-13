a1964d6e82a79b1fb82e7ddbf893c24c184b6fa2
Henry Box <14365005+henrybox@users.noreply.github.com>
2024-12-13 19:17:49 -0300

    feat: realtime agents implementation

--- modules/realtime/agents/v2/index.ts
+++ modules/realtime/agents/v2/index.ts
@@ -1,3 +1,22 @@
+import type { ISessionSettings } from '@aimpact/agents-api/realtime/agents/base';
 import { BaseRealtimeAgent } from '@aimpact/agents-api/realtime/agents/base';
 
-export /*bundle*/ class AgentV2 extends BaseRealtimeAgent {}
+export /*bundle*/ class AgentV2 extends BaseRealtimeAgent {
+	constructor(settings: ISessionSettings) {
+		super(settings);
+	}
+
+	async connect(): Promise<boolean> {
+		const connected = super.connect();
+		if (!connected) return false;
+
+		/**
+		 * CONTINUE HERE!
+		 */
+		// await Chat.fetch();
+
+		// this.session.update();
+
+		return true;
+	}
+}
